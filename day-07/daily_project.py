99df08fbf44a8b25bf2d75a2aa00664b10f97ae1
Itay Vazana <vazanaitay3133@gmail.com>
2025-03-07 23:58:17 +0200

    day 7 - Complete Daily Project Include Graphic

--- day-07/daily_project.py
+++ day-07/daily_project.py
@@ -5,6 +5,7 @@ import random
 words = ["apple", "mouse", "bedroom", "fire", "lamp", "software", "elephant"]
 lives = 6
 rand_word = random.choice(words)
+art_index = 0
 
 placeholder = []
 for i in range(0, len(rand_word)):
@@ -12,19 +13,100 @@ for i in range(0, len(rand_word)):
 
 letter_exist = False
 win_flag = False
+already_exist = False
 
 display = ""
 for i in range(0, len(placeholder)):
     display += placeholder[i]
 
-while not win_flag and lives > 0:
-    print(f"----------> {display} <----------")
-    letter = input(f"insert your guess").lower()
+gui_1 = (
+         f"               _______                   \n"
+         f"               |      |                  \n"
+         f"                      |                  \n"
+         f"                     _|_                 \n"
+         f"                    [ | ]                \n"
+         f" ♥ = 6              ^ ^ ^                \n"
+         f"*****************************************")
 
-    for i in range(0, len(rand_word)):
-        if rand_word[i] == letter:
-            placeholder[i] = letter
-            letter_exist = True
+gui_2 = (
+         f"               _______                    \n"
+         f"               |      |                   \n"
+         f"               O      |                   \n"
+         f"                     _|_                  \n"
+         f"                    [ | ]                 \n"
+         f" ♥ = 5              ^ ^ ^                 \n"
+         f"******************************************\n")
+
+gui_3 = (
+         f"               _______                    \n"
+         f"               |      |                   \n"
+         f"               O      |                   \n"
+         f"               |     _|_                  \n"
+         f"                    [ | ]                 \n"
+         f" ♥ = 4              ^ ^ ^                 \n"
+         f"******************************************\n")
+
+gui_4 = (
+         f"               _______                    \n"
+         f"               |      |                   \n"
+         f"               O      |                   \n"
+         f"             #-|     _|_                  \n"
+         f"                    [ | ]                 \n"
+         f" ♥ = 3              ^ ^ ^                 \n"
+         f"******************************************\n")
+
+gui_5 = (
+         f"               _______                    \n"
+         f"               |      |                   \n"
+         f"               O      |                   \n"
+         f"             #-|-#   _|_                  \n"
+         f"                    [ | ]                 \n"
+         f" ♥ = 2              ^ ^ ^                 \n"
+         f"******************************************\n")
+
+gui_6 = (
+         f"               _______                    \n"
+         f"               |      |                   \n"
+         f"               O      |                   \n"
+         f"             #-|-#   _|_                  \n"
+         f"                L   [ | ]                 \n"
+         f" ♥ = 1              ^ ^ ^                 \n"
+         f"******************************************\n")
+
+gui_lose = (
+            f"               _________                   \n"
+            f"               |       |                   \n"
+            f"               O       |                   \n"
+            f"             #-|-#    _|_                  \n"
+            f"              J L    [ | ]                 \n"
+            f" ♥ = 0               ^ ^ ^      You Lose !!\n"
+            f"******************************************\n")
+
+gui_win = (f"******************************************\n"
+           f"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+           f"    ♥ Y O U                               \n" 
+           f"           ♥ A R E                        \n"
+           f"                ♥ T H E                   \n"
+           f"                      ♥  W I N N E R !!   \n"
+           f"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+           f"******************************************")
+
+gui_art = [gui_1, gui_2, gui_3, gui_4, gui_5, gui_6]
+
+
+while not win_flag and lives > 0:
+    print(f"*******************************************\n"
+          f"The word was ---->         {display}       \n")
+    print(gui_art[art_index])
+    letter = input(f"insert your guess : ").lower()
+
+    if display.__contains__(letter):
+        already_exist = True
+    else:
+        for i in range(0, len(rand_word)):
+            if rand_word[i] == letter:
+                placeholder[i] = letter
+                letter_exist = True
 
     display = ""
     for i in range(0, len(placeholder)):
@@ -32,21 +114,24 @@ while not win_flag and lives > 0:
     if display == rand_word:
         win_flag = True
 
+    print("~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-")
     if win_flag:
-        print("The Word is:")
-        print(f"----------> {display} <----------")
-        print(f"Bravo! you complete the word!")
+        print(f"The Word is:    {display}")
+        print(gui_win)
+    elif already_exist:
+        print(f"You already discover that letter - try another!")
+        already_exist = False
     elif letter_exist:
-        print(f"Success! this was a good guess..")
+        print(f"Good job! this was a great guess!")
         letter_exist = False
     elif not letter_exist and lives == 1:
+        print(f"The Word was:    {rand_word}")
         lives -= 1
-        print("The Word was:")
-        print(f"----------> {rand_word} <----------")
-        print(f"but you didn't succeeded to guess all letters! you lose the game!")
+        print(gui_lose)
     else:
         lives -= 1
-        print(f"Oops! wrong guess.. you left with {lives} lives")
+        art_index += 1
+        print(f"Oops! wrong guess..")
 
 
 
