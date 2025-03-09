351d399c84ab25c5d6018c419178858038bb4f75
Itay Vazana <vazanaitay3133@gmail.com>
2025-03-09 13:31:11 +0200

    Day 8 - daily procject in process(Caesar Cypher)

---
+++ day-08/daily_project.py
@@ -0,0 +1,97 @@
+# Day 8 - Caesar Cipher Generator
+
+
+gui_title = """
+        CCCCCCCCCCCCC                                                                                                                                          
+     CCC::::::::::::C                                                                                                                                          
+   CC:::::::::::::::C                                                                                                                                          
+  C:::::CCCCCCCC::::C                                                                                                                                          
+ C:::::C       CCCCCC  aaaaaaaaaaaaa       eeeeeeeeeeee        ssssssssss     aaaaaaaaaaaaa   rrrrr   rrrrrrrrr                                                 
+C:::::C                a::::::::::::a    ee::::::::::::ee    ss::::::::::s    a::::::::::::a  r::::rrr:::::::::r                                               
+C:::::C                aaaaaaaaa:::::a  e::::::eeeee:::::eess:::::::::::::s   aaaaaaaaa:::::a r:::::::::::::::::r                                              
+C:::::C                         a::::a e::::::e     e:::::es::::::ssss:::::s           a::::a rr::::::rrrrr::::::r                                             
+C:::::C                  aaaaaaa:::::a e:::::::eeeee::::::e s:::::s  ssssss     aaaaaaa:::::a  r:::::r     r:::::r                                             
+C:::::C                aa::::::::::::a e:::::::::::::::::e    s::::::s        aa::::::::::::a  r:::::r     rrrrrrr                                             
+C:::::C               a::::aaaa::::::a e::::::eeeeeeeeeee        s::::::s    a::::aaaa::::::a  r:::::r                                                          
+ C:::::C       CCCCCCa::::a    a:::::a e:::::::e           ssssss   s:::::s a::::a    a:::::a  r:::::r                                                          
+  C:::::CCCCCCCC::::Ca::::a    a:::::a e::::::::e          s:::::ssss::::::sa::::a    a:::::a  r:::::r                                                          
+   CC:::::::::::::::Ca:::::aaaa::::::a  e::::::::eeeeeeee  s::::::::::::::s a:::::aaaa::::::a  r:::::r                                                          
+     CCC::::::::::::C a::::::::::aa:::a  ee:::::::::::::e   s:::::::::::ss   a::::::::::aa:::a r:::::r                                                          
+        CCCCCCCCCCCCC  aaaaaaaaaa  aaaa    eeeeeeeeeeeeee    sssssssssss      aaaaaaaaaa  aaaa rrrrrrr                                                          
+                                                          CCCCCCCCCCCCC  iiii                      hhhhhhh                                                     
+                                                       CCC::::::::::::C i::::i                     h:::::h                                                     
+                                                     CC:::::::::::::::C  iiii                      h:::::h                                                     
+                                                    C:::::CCCCCCCC::::C                            h:::::h                                                     
+                                                   C:::::C       CCCCCCiiiiiii ppppp   ppppppppp    h::::h hhhhh           eeeeeeeeeeee    rrrrr   rrrrrrrrr   
+                                                  C:::::C              i:::::i p::::ppp:::::::::p   h::::hh:::::hhh      ee::::::::::::ee  r::::rrr:::::::::r  
+                                                  C:::::C               i::::i p:::::::::::::::::p  h::::::::::::::hh   e::::::eeeee:::::eer:::::::::::::::::r 
+                                                  C:::::C               i::::i pp::::::ppppp::::::p h:::::::hhh::::::h e::::::e     e:::::err::::::rrrrr::::::r
+                                                  C:::::C               i::::i  p:::::p     p:::::p h::::::h   h::::::he:::::::eeeee::::::e r:::::r     r:::::r
+                                                  C:::::C               i::::i  p:::::p     p:::::p h:::::h     h:::::he:::::::::::::::::e  r:::::r     rrrrrrr
+                                                  C:::::C               i::::i  p:::::p     p:::::p h:::::h     h:::::he::::::eeeeeeeeeee   r:::::r            
+                                                   C:::::C       CCCCCC i::::i  p:::::p    p::::::p h:::::h     h:::::he:::::::e            r:::::r            
+                                                    C:::::CCCCCCCC::::Ci::::::i p:::::ppppp:::::::p h:::::h     h:::::he:::::::e           r:::::r            
+                                                     CC:::::::::::::::Ci::::::i p::::::::::::::::p  h:::::h     h:::::h e::::::::eeeeeeee   r:::::r            
+                                                       CCC::::::::::::Ci::::::i p::::::::::::::pp   h:::::h     h:::::h  ee:::::::::::::e   r:::::r            
+                                                          CCCCCCCCCCCCCiiiiiiii p::::::pppppppp     hhhhhhh     hhhhhhh    eeeeeeeeeeeeee   rrrrrrr            
+                                                                                p:::::p                                                                        
+                                                                                p:::::p                                                                        
+                                                                               p:::::::p                                                                       
+                                                                               p:::::::p                                                                       
+                                                                               p:::::::p                                                                       
+                                                                               ppppppppp                                                                       
+"""
+
+def encode(msg, shift_num):
+    msg = msg.lower()
+    encrypted_msg = ""
+    alphabet = " abcdefghijklmnopqrstuvwxyz"
+    for letter in msg:
+        for i in range(len(alphabet)):
+            if letter == alphabet[i]:
+                c = i + shift_num
+                while c >= len(alphabet):
+                    c -= len(alphabet)
+                encrypted_msg += alphabet[c]
+
+    print(f"The encoded result (between the []) : [{encrypted_msg}] ")
+
+
+def decode(msg, shift_num):
+    original_msg = ""
+    alphabet = " abcdefghijklmnopqrstuvwxyz"
+    for letter in msg:
+        for i in range(len(alphabet)):
+            if letter == alphabet[i]:
+                c = i - shift_num
+                while c < 0:
+                    c += len(alphabet)
+                original_msg += alphabet[c]
+
+    print(f"The decoded result : {original_msg}")
+
+
+p_flag = True
+
+print(gui_title)
+while p_flag:
+    operation = input("Type 'encode' to encrypt or 'decode' to decrypt:\n").lower()
+    while True:
+        if operation == "encode" or operation == "decode":
+            break
+        else:
+            print("Invalid input - try again!")
+            operation = input("Type 'encode' to encrypt or 'decode' to decrypt:\n").lower()
+
+    massage = input("Type your massage:\n")
+    shift_number = int(input("Type the shift number:\n"))
+
+    if operation == "encode":
+        encode(msg=massage, shift_num=shift_number)
+    else:
+        decode(msg=massage, shift_num=shift_number)
+
+    more_op = input("To continue , type 'yes' , otherwise type somthing else..").lower()
+    if more_op != 'yes':
+        p_flag = False
+
--- day-08/main.py
+++ day-08/main.py
@@ -1,7 +1,54 @@
 # Day 8 - 100 Days of Code - Python
 
-def main():
-    print('Hello from Day 8!')
+# functions with input
+# Caesar Cipher - the daily project
+# converting using shift number
 
-if __name__ == '__main__':
-    main()
+def greet():
+    for i in range(3):
+        print("Hello World!")
+
+
+greet()
+
+# what if we want to greet someone specific?
+# what if we want to pass a name to the function and use it?
+# we can do it by using a parameter to the function.
+# def function(*insert here*)
+# example:
+
+def greet_someone(specific_name):
+    print(f"Hello {specific_name} , how are you?")
+
+
+someone = "David"
+greet_someone(someone)
+
+# or even do it with list :
+names = ["Mishel", "David", "Itay", "Koral", "Donald"]
+for name in names:
+    greet_someone(name)
+
+# function with more then 1 input:
+def greet_with_name_and_loc(specific_name,specific_location):
+    print(f"Hello {specific_name}")
+    print(f"What is it like in {specific_location}")
+
+
+locations = ["Finland", "Tokyo", "Tel-Aviv", "California", "Paris"]
+
+i = 0
+for name in names:
+    greet_with_name_and_loc(name,locations[i])
+    i += 1
+
+# we can ensure the position of each string as a parameter by using keys :
+i = 0
+for name in names:
+    greet_with_name_and_loc(specific_name=name,specific_location=locations[i])
+    i += 1
+
+# order is important - especially when working with lots of parameters.
+
+
+
