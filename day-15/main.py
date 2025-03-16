238351717fb12b7d1bd1655a143381463619c751
Itay Vazana <vazanaitay3133@gmail.com>
2025-03-16 13:55:37 +0200

    Day 15 - Starting daily project - a Coffe machine

--- day-15/main.py
+++ day-15/main.py
@@ -1,7 +1,41 @@
 # Day 15 - 100 Days of Code - Python
 
-def main():
-    print('Hello from Day 15!')
+# Today we move from Beginner level
+# to Intermediate level
 
-if __name__ == '__main__':
-    main()
+# The daily project is Coffe machine
+# the coffe machine can create more than one
+# type of drink - each one with different amount
+# of ingredients - milk , water and more..
+
+# another think  that we need to implement is
+# the fact that it's coin operated
+# there are 4 types of coins (in US)
+
+# Penny = 0.01 (1 cent)
+# Nickel = 0.05 (5 cent)
+# Dime = 0.1 (10 cent)
+# Quarter = 0.25 (25 cent)
+
+# what is the program requirements :
+
+# 1. Print a report
+# (It's need to say how many of each ingredient last,
+# and what possible type of drinks we can order)
+
+# 2. Check resources sufficient
+# after each order we need to make sure we update
+# the amount of each ingredient and also the money is
+# the box inside the machine
+# if it's not possible to make a specific order , it
+# should warn  when try to order it.
+
+# 3. Process coins
+# we need to make sure we refund the money if there
+# isn't enough from it.
+# if the value of money is more then the cost - we need
+# to calculate the change and return it.
+
+# 4. Check transaction successful
+# if false - refund and not make coffe
+# if true - make the coffe and update every thing
