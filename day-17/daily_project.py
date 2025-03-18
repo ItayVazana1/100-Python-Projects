b34dfd84e7be41b21c2edc245d4ecd9921379405
Itay Vazana <vazanaitay3133@gmail.com>
2025-03-19 00:13:02 +0200

    Day 17 - fix a logic issue - now works perfect

--- day-17/daily_project.py
+++ day-17/daily_project.py
@@ -39,10 +39,10 @@ class Player:
 
     def update_score(self, is_correct):
         if is_correct:
-            print("Correct!")
+            print("\nCorrect!")
             self.score += 1
         else:
-            print("Wrong! - lose 1 live..")
+            print("\nWrong! - lose 1 live..")
             self.lives -= 1
             print(f"You have {self.lives} lives remain..")
 
@@ -55,8 +55,8 @@ class Player:
 
     def game_statics(self):
         print("****************")
-        print(f"Player name = {self.name}")
-        print(f"Score - {self.score}")
+        print(f"Player name : {self.name}")
+        print(f"Score : {self.score}")
         print("****************")
 
 
@@ -72,7 +72,7 @@ for q_set in statements:
 print(art)
 input("Type any key to start..")
 
-player_name = input("Inser your name:\n")
+player_name = input("Insert your name:\n")
 player = Player(player_name)
 
 print("And the game begin!")
@@ -80,6 +80,7 @@ print("----------------------------------------------------")
 
 while game_flag:
     print("\n")
+    random.shuffle(questions_bank)
     current_q = random.choice(questions_bank)
     current_q.show(question_counter)
     ans = input("").lower()
@@ -88,17 +89,25 @@ while game_flag:
         ans = input("Choose - True or False?\n").lower()
     result = current_q.check_if_correct(convert_ans_boolean(ans))
     player.update_score(result)
-    if not player.check_if_lose():
-        print("I am sorry..You lose!")
+    if player.check_if_lose():
+        print("\nI am sorry..You lose!")
         print("Here is your statistics:")
         game_flag = False
         break
     ask_if_quit = input("\nType 'Q' to Quit now , otherwise type any key when you ready for the next Question..").lower()
     if ask_if_quit == 'q':
-        print("It's Ok! - you played well!")
+        print("\nIt's Ok! - you played well!")
         print("Here is your statistics:")
-        Player.game_statics()
+        player.game_statics()
         game_flag = False
+    else:
+        question_counter += 1
+        questions_bank.remove(current_q)
 
+    if len(questions_bank) == 0:
+        print("No more questions in the bank - you are so smart!")
+        print("I hope you have fun!")
+        print("Bye Bye!")
+        game_flag = False
 
 
