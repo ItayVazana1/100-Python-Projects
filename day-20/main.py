e6712e7067dfbdf1ff44774ad19e191073ab36c2
Itay Vazana <vazanaitay3133@gmail.com>
2025-03-26 22:39:53 +0200

    Day 20 - Today's project is the first part of sanke game - make some progress in that .. I will continue tomorrow

--- day-20/main.py
+++ day-20/main.py
@@ -1,7 +1,50 @@
 # Day 20 - 100 Days of Code - Python
 
-def main():
-    print('Hello from Day 20!')
+from turtle import Turtle, Screen
 
-if __name__ == '__main__':
-    main()
+
+snake = []
+
+def move_with_next(distance):
+    save_pos = []
+    for t in snake:
+        save_pos.append([t.xcor(), t.ycor()])
+
+    for i in range(len(save_pos)):
+        save_pos[i][0] += distance
+
+    for i in range(len(snake)):
+        snake[i].setx(save_pos[i][0])
+
+
+
+def load_turtles():
+    x = 0
+    for i in range(6):
+        new_t = Turtle()
+        new_t.pu()
+        new_t.shape("circle")
+        new_t.color("green")
+        new_t.setx(x)
+        x -= 20
+        snake.append(new_t)
+
+
+def move_forward():
+
+    move_with_next(10)
+
+
+
+load_turtles()
+
+
+screen = Screen()
+screen.listen()
+
+while True:
+    move_forward()
+
+
+
+screen.exitonclick()
